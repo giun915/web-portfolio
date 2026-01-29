b627666abcafae75355f4dae598f1ccd6f2750b3
DESKTOP-61V2IID\gigiu <giun915@gmail.com>
2026-01-29 10:41:13 +0900

    feat: convert header and footer to Vue components

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/css/default.css'
+import './assets/css/animation.css'
+import './assets/css/layout.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
