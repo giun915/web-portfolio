7cb00c23015108520aca4ff8ee40ed2231c52f91
DESKTOP-61V2IID\gigiu <giun915@gmail.com>
2026-02-24 11:34:46 +0900

    chore: add GA gtag TypeScript declarations

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+export {}
+
+declare global {
+  interface Window {
+    dataLayer: unknown[]
+    gtag: (
+      command: 'config' | 'event' | 'js',
+      targetId: string | Date,
+      config?: Record<string, unknown>,
+    ) => void
+  }
+}
